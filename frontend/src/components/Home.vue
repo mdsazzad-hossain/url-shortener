51bed20ba561afb21152f6b5cfeead1563205786
mdsazzad-hossain <mdsojib508@gmail.com>
2022-03-18 13:35:03 +0600

    modal add

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,7 +5,7 @@
       <h3>Paste the url to be shortened</h3>
     </div>
     <div class="col-4 offset-4 p-2 text-right d-flex div-position">
-      <button class="btn btn-outline-primary">Sign In</button>
+      <button type="button" @click="visibleModal()" class="btn btn-outline-primary">Sign In</button>
       <button class="btn btn-outline-primary">Sign Up</button>
     </div>
     <div class="d-flex text-center col-8 offset-2">
@@ -15,14 +15,29 @@
       </div>
       <button class="btn btn-success">Submit</button>
     </div>
+      <login ref="modal"/>
+    <!-- <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true">
+    </div> -->
   </div>
 </template>
 
 <script>
+import Login from './Login.vue'
+import Register from './Register.vue'
 export default {
+  components:{
+    Login,
+    Register
+  },
   data () {
     return {
       url: ''
+    }
+  },
+  methods:{
+    visibleModal () {
+      console.log('test')
+      this.$refs.modal.show()
     }
   }
 }
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    v-if="showModal"
+  >
+    <div id="modal" class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">...</div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+     return {
+      showModal: false
+     }
+   },
+   methods: {
+     show() {
+      this.showModal = true
+     },
+     hide(){
+      this.showModal = false
+     }
+   }
+ }
+</script>
+
+<style></style>
